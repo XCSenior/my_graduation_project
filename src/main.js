3bc8f9945cae8b2b945babbcb0d1d7479c372ac2
XCSenior <2969271422@qq.com>
2022-01-19 00:45:08 +0800

    add router and ShoppingMall , delete HelloWorld.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Button,Row,Col} from 'vant';
+/* 引入写好的路由器 */
+import router from './router/index';
 
 Vue.config.productionTip = false
+Vue.use(Button).use(Row).use(Col)
+
 
 new Vue({
 	render: h => h(App),
+	router,
+	components:{App}
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import ShoppingMall from '@/components/pages/ShoppingMall';
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+	routes:[
+		{ path:"/", name:"ShoppingMall", component:ShoppingMall}
+	]
+})
