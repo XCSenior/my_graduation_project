a7305b439ca7d8562f94a0cc39038451c8a78c72
XCSenior <2969271422@qq.com>
2022-01-19 17:09:33 +0800

    add search-bar

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,10 +1,18 @@
 <template>
 	<div>
-		<van-row class="test-row">
-			<van-col span="8">span:8</van-col>
-			<van-col span="8">span:8</van-col>
-			<van-col span="8">span:8</van-col>
-		</van-row>
+		<div class="search-bar">
+			<van-row>
+				<van-col span="3">
+					<img :src="locationIcon" width="80%" alt="定位.png" class="location-icon">
+				</van-col>
+				<van-col span="16">
+					<input type="text" class="search-input">
+				</van-col>
+				<van-col span="5" class="search-button">
+					<van-button size="mini">查找</van-button>
+				</van-col>
+			</van-row>
+		</div>
 	</div>
 </template>
 
@@ -13,14 +21,32 @@ export default {
 	name:"ShoppingMall",
 	data() {
 		return {
-			msg:"ShoppingMall"
+			msg:"ShoppingMall",
+			locationIcon:require("../../assets/images/location.png")
 		}
 	},
 }
 </script>
 
 <style scoped>
-	.test-row{
-		text-align: center;
+	.search-bar{
+		height: 2.5rem;
+		background-color: #e5017b;
+		line-height: 2.5rem;	/* 文字上下居中 */
+	}
+	.search-input{
+		width: 100%;
+		height: 1.3rem;
+		border: 0;
+		border-bottom: 1px solid white !important;
+		background-color: #e5017b;
+		color: white;		/* 字体颜色 */
+	}
+	.location-icon{
+		padding-top: 0.2rem;
+		padding-left: 0.3rem;
+	}
+	.search-button{
+		padding-left: 0.5rem;
 	}
 </style>
