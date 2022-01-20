f65325455fec7975298bc149041a91b520acebfb
新城学长 <2969271422@qq.com>
2022-01-21 01:14:28 +0800

    增加广告条，改进获取轮播图图片的路径为JSON数据获取

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -15,9 +15,9 @@
 		</div>
 		<!-- 轮播图swiper area-->
 		<div class="swiper-area">
-			<van-swipe :autoplay="1000">
+			<van-swipe :autoplay="2000">
 				<van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
-					<a href="#"><img v-lazy="banner.imageUrl" width="100%" alt="banner"></a>
+					<a href="#"><img v-lazy="banner.image" width="100%" alt="banner"></a>
 				</van-swipe-item>
 			</van-swipe>
 		</div>
@@ -29,6 +29,10 @@
 				<span>{{cate.mallCategoryName}}</span>
 			</div>
 		</div>
+		<!-- 广告条adBanner -->
+		<div class="adBanner-area">
+			<img v-lazy="adBanner" alt="广告banner.gif" width="100%">
+		</div>
 	</div>
 </template>
 
@@ -39,12 +43,9 @@ export default {
 	data() {
 		return {
 			locationIcon:require("../../assets/images/location.png"),
-			bannerPicArray:[
-				{imageUrl:require("../../assets/images/simleVueDemoPic001.jpg")},
-				{imageUrl:require("../../assets/images/simleVueDemoPic002.jpg")},
-				{imageUrl:require("../../assets/images/simleVueDemoPic003.jpg")}
-			],
-			category:[]
+			bannerPicArray:[],
+			category:[],
+			adBanner:""
 		}
 	},
 	created() {
@@ -52,16 +53,22 @@ export default {
 		axios({
 			url:"http://127.0.0.1:4000/index",
 			method:"get"
-		}).then((response) => {
+		})
+		.then((response) => {
 			console.log(response.data);			/* 打印data */
 			/* 如果请求成功 */
 			if(response.status === 200){
+				/* 1、获得type-bar的目录数据 */
 				this.category = response.data.data.category;
 				/* 由于type-bar图片第一张大小不同,所以下载至本地。现在遍历改成本地路径 */
 				this.category.forEach( (cate,index)=>{
 					cate.image = require(`../../assets/images/type-bar/${index+1}.png`)
 				});
 				// console.log(this.category);
+				/* 2、获得广告条图片 */
+				this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
+				/* 3、获取轮播图的image */
+				this.bannerPicArray = response.data.data.slides;
 			}
 		}).catch((error) => {
 			console.log(error);
@@ -94,7 +101,7 @@ export default {
 	}
 	.swiper-area{
 		clear: both;	/* 清除浮动 */
-		max-height: 17.5rem;	/* 设置最大高度，不让低网速时未知图片高度，组件溢出 */
+		max-height: 9rem;	/* 设置最大高度，不让低网速时未知图片高度，组件溢出 */
 		overflow: hidden;	/* 组件溢出隐藏 */
 	}
 	.type-bar{
