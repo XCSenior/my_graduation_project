cca2197eb060a15a449ba75fbc0a8f7bf6a41a32
新城学长 <2969271422@qq.com>
2022-02-13 20:07:35 +0800

    增加koa2用户操作的路由模块化功能

---
+++ service/appApi/user.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+
+let router = new Router();
+
+router.get("/", async (ctx) => {
+	ctx.body="这是用户操作首页";
+})
+
+router.get("/register", async (ctx) => {
+	ctx.body="用户注册接口";
+})
+
+/* 暴露router */
+module.exports = router;
+
--- service/index.js
+++ service/index.js
@@ -1,9 +1,21 @@
 /* 后台服务器index */
 const mongoose = require('mongoose');
 const Koa = require("koa");
+const Router = require("koa-router");
+const user = require("./appApi/user");
+
 const app = new Koa();
 /* 数据库连接connect函数对象,初始化Schema函数 */
 const { connect , initSchemas } = require('./database/init.js');
+
+
+/* 装载所有子路由 */
+let router = new Router();
+router.use("/user" , user.routes());
+// 路由中间件
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 
 app.use(async (ctx)=>{
 	ctx.body = `<h1>Hello Koa2</h1>`;
@@ -16,26 +28,26 @@ app.listen(3000,() => {
 
 /* 数据库立即执行函数 */
 // 增加获取所有Schema，遍历后引入，并加载进服务中，然后载入模型后进行增查操作
-console.log("启动数据库init服务");
-(async function toConnectDatabase(){
-	await connect();
-	initSchemas();	//载入schemas
-	// 插入一条数据测试
-	const User = mongoose.model("User");	//引入模型
-	let oneUser = new User({
-		userName:"sb01",
-		password:"123456"
-	});
-	oneUser.save().then((result) => {
-		console.log("插入oneUser成功");
-	}).catch((err) => {
-		console.warn("插入oneUser失败");
-	});
+// console.log("启动数据库init服务");
+// (async function toConnectDatabase(){
+// 	await connect();
+// 	initSchemas();	//载入schemas
+// 	// 插入一条数据测试
+// 	const User = mongoose.model("User");	//引入模型
+// 	let oneUser = new User({
+// 		userName:"sb01",
+// 		password:"123456"
+// 	});
+// 	oneUser.save().then((result) => {
+// 		console.log("插入oneUser成功");
+// 	}).catch((err) => {
+// 		console.warn("插入oneUser失败");
+// 	});
 
-	// // 查询数据测试
-	// let user = await User.findOne({}).exec();
-	// console.log("---------------------");
-	// console.log(user);
-	// console.log("---------------------");
-})();
+// 	// // 查询数据测试
+// 	// let user = await User.findOne({}).exec();
+// 	// console.log("---------------------");
+// 	// console.log(user);
+// 	// console.log("---------------------");
+// })();
 
