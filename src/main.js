f57437373d94fa269917633573468c95d315a020
新城学长 <2969271422@qq.com>
2022-02-13 19:41:45 +0800

    增加用户注册页面Register.vue

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 /* 引入写好的路由器 */
 import router from './router/index';
-import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List} from 'vant';
+import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List,Field,NavBar} from 'vant';
 
 
 Vue.config.productionTip = false
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload)
-.use(List)
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar);
 
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ShoppingMall from '@/components/pages/ShoppingMall';
+import Register from '../components/pages/Register';
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
 	routes:[
-		{ path:"/", name:"ShoppingMall", component:ShoppingMall}
+		{ path:"/", name:"ShoppingMall", component:ShoppingMall},
+		{ path:"/register", name:"Register", component:Register}
 	]
 })
