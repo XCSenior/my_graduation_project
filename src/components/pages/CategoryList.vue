00a1efd43b148518acf8c83a7ddb304578b178f0
新城学长 <2969271422@qq.com>
2022-02-16 16:21:13 +0800

    增加商品大类样式，和点击交互效果

--- src/components/pages/CategoryList.vue
+++ src/components/pages/CategoryList.vue
@@ -7,7 +7,13 @@
 			<van-row>
 				<van-col span="6">
 					<div id="leftNav">
-						左侧导航
+						<ul>
+							<li @click="clickCategory(index)"
+								:class="{categoryActive:categoryIndex === index}"
+								v-for="(item, index) in category" :key="item.ID">
+								{{item.MALL_CATEGORY_NAME}}
+							</li>
+						</ul>
 					</div>
 				</van-col>
 				<van-col span="18">
@@ -26,8 +32,18 @@
 	import {Toast} from 'vant';
 	export default {
 		name:"CategoryList",
+		data() {
+			return {
+				category:[],
+				categoryIndex:0,
+			}
+		},
 		created() {
 			this.getCategory();
+		},
+		mounted() {
+			let windowHeight = document.documentElement.clientHeight;
+			document.getElementById("leftNav").style.height = windowHeight - 46 + "px";
 		},
 		methods: {
 			getCategory(){
@@ -37,13 +53,16 @@
 				}).then((response) => {
 					console.log("我是getCategory的response",response);
 					if (response.data.code === 200 && response.data.message) {
-						
+						this.category = response.data.message;
 					} else {
 						Toast.fail("获取数据失败");
 					}
 				}).catch((err) => {
 					console.log("我是getCategory的err",err);
 				});
+			},
+			clickCategory(index){
+				this.categoryIndex = index;
 			}
 		},
 	};
@@ -51,6 +70,16 @@
 
 <style scoped>
 	#leftNav{
-		background-color: blue;
+		background-color: aliceblue;
+	}
+	#leftNav ul li{
+		line-height: 2rem;
+		border-bottom: 1px solid #E4E7ED;
+		padding: 3px;
+		font-size: 0.8rem;
+		text-align: center;
+	}
+	.categoryActive{
+		background-color: #fff;
 	}
 </style>
