55629d07e1b556c77a41772525c4fd6d3c193a6d
新城学长 <2969271422@qq.com>
2022-02-17 18:59:54 +0800

    增加底部导航栏和子导航，并修改路由配置，但有底部导航栏会吃掉子页面高度的bug

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,22 @@ import Login from '../components/pages/Login';
 import Goods from '../components/pages/Goods.vue';
 import CategoryList from '../components/pages/CategoryList.vue';
 import Cart from '../components/pages/Cart.vue';
+import Main from '../components/pages/Main.vue';
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
 	routes:[
-		{ path:"/", name:"ShoppingMall", component:ShoppingMall},
-		{ path:"/register", name:"Register", component:Register},
-		{ path:"/login", name:"Login", component:Login},
-		{ path:"/goods", name:"Goods", component:Goods},
-		{ path:"/CategoryList", name:"CategoryList", component:CategoryList},
-		{ path:"/Cart", name:"Cart", component:Cart},
+		{
+			path:"/", name:"Main", component:Main,
+			children:[
+				{	path:"/ShoppingMall", name:"ShoppingMall", component:ShoppingMall},
+				{	path:"/CategoryList", name:"CategoryList", component:CategoryList},
+				{	path:"/Cart", name:"Cart", component:Cart},
+			]
+		},
+		{	path:"/register", name:"Register", component:Register},
+		{	path:"/login", name:"Login", component:Login},
+		{	path:"/goods", name:"Goods", component:Goods},
 	]
 })
