1bd5cb70586f93d8358f60b1e9a63e8128768efd
新城学长 <2969271422@qq.com>
2022-02-17 14:39:56 +0800

    增加购物车页面的初步搭建，以及页面路由的初步配置

---
+++ src/components/pages/Cart.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div class="navbar-div">
+			<van-nav-bar
+				title="购物车"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"Cart",
+	data() {
+		return {
+			cartInfo:[],	//加入的商品
+			isEmpty:false,	//判断是否为空
+		}
+	},
+	created() {
+		this.getCartInfo();
+	},
+	methods: {
+		/* 得到购物车信息 */
+		getCartInfo(){
+			if(localStorage.cartInfo){
+				this.cartInfo = JSON.parse(localStorage.cartInfo);
+			}
+			console.log("我是cartInfo",this.cartInfo);
+			this.isEmpty = this.cartInfo.length > 0 ? false : true;
+		}
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Register from '../components/pages/Register';
 import Login from '../components/pages/Login';
 import Goods from '../components/pages/Goods.vue';
 import CategoryList from '../components/pages/CategoryList.vue';
+import Cart from '../components/pages/Cart.vue';
 
 Vue.use(VueRouter)
 
@@ -15,5 +16,6 @@ export default new VueRouter({
 		{ path:"/login", name:"Login", component:Login},
 		{ path:"/goods", name:"Goods", component:Goods},
 		{ path:"/CategoryList", name:"CategoryList", component:CategoryList},
+		{ path:"/Cart", name:"Cart", component:Cart},
 	]
 })
